a463a011bd83bdd295a0b5837f5be59a92210799
brunau1 <brunosantosprotec@gmai.com>
2019-05-22 11:40:19 -0300

    created the data structure controller

--- src/controllers/DataStructureController.ts
+++ src/controllers/DataStructureController.ts
@@ -1,2 +1,11 @@
 import DataStructureInterface from "../models/DataStructureInterface"
 
+const dataStructure: DataStructureInterface[] = []
+
+for (let i = 0; i < 60; i++) {
+    dataStructure[i].betNumber = i + 1
+    dataStructure[i].contAppeared.appearGlobalContext = 0
+    for (let j = 0; j < 6; j++) {
+        dataStructure[i].contAppeared.appearPerCase[j] = 0
+    }
+}

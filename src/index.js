63bab0b2f3a5b6f61b0ae3fa4165c5979433f2e9
brunau1 <brunosantosprotec@gmail.com>
2022-04-20 20:33:24 -0300

    feat: todo comment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 const extractData = require("./data-extractor");
 
 extractData();
+// TODO - apply probability to each number in raffle result to predict winner
+// the probability is based in the number of times the number appears in the raffle result
+// for example, if the number appears 5 times in a total of 10 raffle results, the probability is 5/10 = 0.5
